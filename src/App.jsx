351401b193d387a28c1192208abcaf680528a48c
Alexander Sinitsyn <coder27sinitsyn@gmail.com>
2026-04-21 09:47:46 +0300

    lesson #1.3
    #time 35m
    1) Как правильно создавать компоненты
    2) jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
+import { MyComponent } from './MyComponent.jsx'
+
+// React компоненты должны начинаться с заглавной буквы, если с маленьков, то react воспринимает как html элемент
+// расширение .jsx - используется синтаксическое расширение JavaScript, позволяющее писать HTML-подобную разметку прямо внутри JavaScript-кода.
+
 export const App = () => {
-  return <h1>Hello React</h1>
+  return (
+    <div>
+      <h1>Hello React</h1>
+      <MyComponent></MyComponent>
+    </div>
+  )
 }
---
+++ src/MyComponent.jsx
@@ -0,0 +1,7 @@
+export const MyComponent = () => {
+  return (
+    <h2>
+      Далеко-далеко за словесными горами
+    </h2>
+  )
+}
